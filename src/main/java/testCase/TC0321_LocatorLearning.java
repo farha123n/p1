afaf3ec97420faea0f6179206b94c31558311577
unknown <you@example.com>
2022-06-11 10:49:18 -0700

    assingment problem

--- src/main/java/testCase/TC0321_LocatorLearning.java
+++ src/main/java/testCase/TC0321_LocatorLearning.java
@@ -3,6 +3,8 @@ package testCase;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import base.DriverSetup;
@@ -18,9 +20,29 @@ public class TC0321_LocatorLearning extends DriverSetup{
 		driver.manage().window().maximize();
 		
 		Thread.sleep(2000);
-		driver.findElement(By.className("ubermenu-target-text")).click();
+		/*driver.findElement(By.className("ubermenu-target-text")).click();
 		Thread.sleep(2000);
-		
+		*/
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		// Scroll to Bottom
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight);");
+		Thread.sleep(5000);
+		// Scroll to Top
+		/*js.executeScript("window.scrollTo(document.body.scrollHeight,0);");
+		Thread.sleep(5000);
+		*/// Scroll to specific Element
+		WebElement e = driver.findElement(By.className("elementor-size-default"));
+		js.executeScript("arguments[0].scrollIntoView();", e);
+		Thread.sleep(5000);
+		driver.findElement(By.className("size-woocommerce_thumbnail")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("cart")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.className("xt_woofcicon-flat-plus")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.className("xt_woofc-footer-label")).click();
+		Thread.sleep(2000);
 	
 	}
 }
